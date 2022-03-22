803f189237f97cab21a01062bc943bb551be6353
michael <michaelmardiansyah@students.amikom.ac.id>
2022-03-22 23:20:44 +0700

    taxi

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Taxi
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            //membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriverName = "MEKEL";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 5;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            Console.WriteLine();
+            taxi.PickupPassenger();
+            taxi.DropOffPassenger();
+            
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ Taxi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Taxi
+{
+    public class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine($"Driver Name: {DriverName}");
+            Console.WriteLine($"On Duty: {(OnDuty ? "Yes" : "No")}");
+            Console.WriteLine($"Number of Passenger: {NumPassenger}");
+        }
+        public void PickupPassenger()
+        {
+            Console.WriteLine($"{DriverName} sedang dalam perjalanan menjemput penumpang");
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine($"{DriverName} driver selesai mengantar penumpang sampai tujuan");
+        }
+    }
+}
